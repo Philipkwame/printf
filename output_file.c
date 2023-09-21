a22dc372de8fd34a340fcfd3555f3d64c1d0715f
Philipkwame <mrphilipacquah@gmail.com>
2023-09-21 16:26:13 -0700

    output_file: we had to correct some function

--- output_file.c
+++ output_file.c
@@ -18,38 +18,38 @@ int process_format(const char *format, c_nvert confun_list[], va_list args)
 
 	for (i = 0; format[i] != '\0'; i++) /* Iterate through the main string */
 	{
-	if (format[i] == '%') /* Check for format specifiers */
-	{
-	/* Iterate through the struct to find the right function */
-	for (j = 0; confun_list[j].fsymbols != NULL; j++)
-	{
-	if (format[i + 1] == confun_list[j].fsymbols[0])
-	{
-	result = confun_list[j].fun(args);
-	if (result == -1)
-	return (-1);
-	output_ch += result;
-	break;
-	}
-	}
-	if (confun_list[j].fsymbols == NULL && format[i + 1] != ' ')
-	{
-	if (format[i + 1] != '\0')
-	{
-	_putchar(format[i]);
-	_putchar(format[i + 1]);
-	output_ch += 2;
-	}
-	else
-	return (-1);
-	}
-	i = i + 1; /* Update i to skip format symbols */
-	}
-	else
-	{
-	_putchar(format[i]); /* Call the _putchar function */
-	output_ch++;
-	}
+		if (format[i] == '%') /* Check for format specifiers */
+		{
+		/* Iterate through the struct to find the right function */
+			for (j = 0; confun_list[j].fsymbols != NULL; j++)
+			{
+				if (format[i + 1] == confun_list[j].fsymbols[0])
+				{
+					result = confun_list[j].fun(args);
+					if (result == -1)
+						return (-1);
+					output_ch += result;
+					break;
+				}
+			}
+			if (confun_list[j].fsymbols == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					output_ch += 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1; /* Update i to skip format symbols */
+		}
+		else
+		{
+			_putchar(format[i]); /* Call the _putchar function */
+			output_ch++;
+		}
 	}
 	return (output_ch);
-	}
+}
